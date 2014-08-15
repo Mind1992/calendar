9c66f9317b2a8f606ff035db942736c2aa04cfb2
Sergii Mertsalov <sergmerts@gmail.com>
2014-08-15 15:38:42 -0700

    set up events table

---
+++ db/migrate/20140815220704_create_events.rb
@@ -0,0 +1,12 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+    	t.column :description, :string
+    	t.column :location, :string
+    	t.column :start_date, :date
+    	t.column :end_date, :date
+    	t.column :start_time, :time 
+    	t.column :end_time, :time  
+    end
+  end
+end
