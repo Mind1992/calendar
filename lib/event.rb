65450604ffd31003cc01b837d77bd92249e45d29
Sergii Mertsalov <sergmerts@gmail.com>
2014-08-15 22:21:19 -0700

    add before_create callback

--- lib/event.rb
+++ lib/event.rb
@@ -1,4 +1,12 @@
 class Event < ActiveRecord::Base
 	validates :description, :presence => true
 	validates :location, :presence => true
+	before_create :capitalize
+
+private
+
+	def capitalize
+		self.description = self.description.capitalize
+		self.location = self.location.capitalize
+	end
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -3,4 +3,9 @@ require 'spec_helper'
 describe Event do 
 	it { should validate_presence_of(:description) }
 	it { should validate_presence_of(:location) }
+	it 'capitalizes the description' do 
+		new_event = Event.create(description: "surfing", location: "smugglers cove")
+		expect(new_event.description).to eq "Surfing"
+		expect(new_event.location).to eq "Smugglers cove"
+	end
 end
